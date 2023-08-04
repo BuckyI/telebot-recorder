1901014c57a79b6b93fe3eb97f0a61adcabd71c7
thor <mightymjolnir@qq.com>
2023-08-04 18:16:26 +0800

    🐞 fix: update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from typing import Final
 
 from decouple import config
 
-import recorderbot
-from recorderbot.bot import bot, recorder
+from recorderbot.bot import bot, storage
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -15,7 +14,7 @@ if __name__ == "__main__":
     HTTPS_PROXY: Final = config("HTTPS_PROXY", default="")
     if HTTPS_PROXY:
         os.environ["https_proxy"] = HTTPS_PROXY
-    num = recorder.merge_from_backup()
-    logging.info("Retrieve {} record(s) from backup.".format(num))
+    # initialize database, restore data from webdav backup
+    storage.restore()
     logging.info("Start Polling...")
     bot.infinity_polling()
